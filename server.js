148b292a29d9edec446d921fb42ab9f046af8e23
Paragax <xagarap@gmail.com>
2024-08-03 19:35:31 -0400

    removed postgres stuff until fully implemented

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const pg = require("pg");
+// const pg = require("pg");
 const express = require("express");
 const app = express();
 
@@ -6,11 +6,11 @@ const port = 3000;
 const hostname = "localhost";
 
 const env = require("./env.json");
-const Pool = pg.Pool;
-const pool = new Pool(env);
-pool.connect().then(function () {
-    console.log(`Connected to database ${env.database}`);
-});
+// const Pool = pg.Pool;
+// const pool = new Pool(env);
+// pool.connect().then(function () {
+//     console.log(`Connected to database ${env.database}`);
+// });
 
 app.use(express.static("public"));
 
