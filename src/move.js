d4c21aa0b2888f945c8cedd61a6c71c0ba9ec2b3
fc387@drexel.edu <fc387@drexel.edu>
2024-08-03 19:02:53 -0400

    Create piece composition layout and move layout
    Created blueprint for future addition
    of move logic and piece composition.

---
+++ src/move.js
@@ -0,0 +1,6 @@
+const moves = {
+    //TODO inplement move logic
+    //Probably functions that check true false
+}
+
+export default moves;
---
+++ src/piece.js
@@ -0,0 +1,13 @@
+const Piece = (starting) => {
+    //TODO: creating piece DOM
+    //TODO: return DOM element
+    let currentPosition = starting;
+
+    const moveTo = (endingPosition) => {
+        //TODO update DOM element positon
+        currentPosition = ending;
+    }
+    return moveTo;
+}
+export default Piece;
+
