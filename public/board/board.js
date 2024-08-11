ecc0a72132616936c70401f303b7b0546bb08fee
fc387@drexel.edu <fc387@drexel.edu>
2024-08-10 20:00:48 -0400

    Added piece movement
    Using movePosition, pieces can now be moved from
    one square to another.
    
    Deleted moves.js because I dont think it is necessary

--- public/board/board.js
+++ public/board/board.js
@@ -1,28 +1,29 @@
 import Knight from '../pieces/knight.js'
 
+let cutOuts = [ { row: 0, col: 0, size: 3 }, { row: 0, col: 11, size: 3 },
+	{ row: 11, col: 0, size: 3 }, { row: 11, col: 11, size: 3 }];
+
 const isCutOut = (r, c) => cutOuts.some(({ row, col, size }) =>
-    r >= row && r < row + size &&
-    c >= col && c < col + size
+	r >= row && r < row + size &&
+	c >= col && c < col + size
 );
 
 let cell = document.getElementById("cell");
-let cutOuts = [ { row: 0, col: 0, size: 3 }, { row: 0, col: 11, size: 3 },
-     { row: 11, col: 0, size: 3 }, { row: 11, col: 11, size: 3 }];
 
 for (let r = 0; r < 14; r++) {
-    for (let c = 0; c < 14; c++) {
-        let div = document.createElement("div");
-        if (isCutOut(r,c)) {
-            div.classList.add("cutout")
-        } else{
-            if ((r + c) % 2 === 0) {
-                div.classList.add("white-cell");
-                div.id = `${r}-${c}`
-            } else {
-                div.classList.add("black-cell");
-                div.id = `${r}-${c}`
-            }
-        }
-        cell.append(div);
-    }
+	for (let c = 0; c < 14; c++) {
+		let div = document.createElement("div");
+		if (isCutOut(r,c)) {
+			div.classList.add("cutout");
+		} else{
+			if ((r + c) % 2 === 0) {
+				div.classList.add("white-cell", "tile");
+				div.id = `${r}-${c}`
+			} else {
+				div.classList.add("black-cell","tile");
+				div.id = `${r}-${c}`
+			}
+		}
+		cell.append(div);
+	}
 };
--- public/pieces/piece.js
+++ public/pieces/piece.js
@@ -1,15 +1,43 @@
 function Piece(starting) {
-    let currentPosition = starting; 
-    let dom = document.createElement("div");
+	let currentPosition = starting;
+	let previousPosition;
+	let dom = document.createElement("div");
 
-    const render = () => {
-        const id = `${currentPosition.row}-${currentPosition.col}`
-        const parent = document.getElementById(`${currentPosition.row}-${currentPosition.col}`)
-        parent.append(dom);
-    }
-    return {
-        dom,
-        render
-    }
+	function getPosition(){
+		return currentPosition;
+	}
+
+	function setPosition(newPosition, boardState){
+		previousPosition = currentPosition;;
+		currentPosition = newPosition;
+		boardState[currentPosition.row][currentPosition.col] = boardState[previousPosition.row][previousPosition.col];
+		boardState[currentPosition.row][currentPosition.col] = 1;
+
+		unrender(previousPosition);
+		render();
+	}
+
+	const render = () => {
+		const id = `${currentPosition.row}-${currentPosition.col}`;
+		const parent = document.getElementById(id);
+		parent.append(dom);
+	}
+
+	const unrender = (position) =>{
+		const id = `${position.row}-${position.col}`;
+		const parent = document.getElementById(id);
+		while (parent.firstChild){
+			parent.removeChild(parent.lastChild);
+		}
+	}
+
+	return {
+		dom,
+		getPosition,
+		setPosition,
+		unrender,
+		render
+	}
 }
 export default Piece;
+
