6ac655b4f39ca8c49231ad54ca6815fe401ef63d
Paragax <xagarap@gmail.com>
2024-08-20 22:10:21 -0400

    added the rest of jason's websocket functionality.

--- app/routes/game.js
+++ app/routes/game.js
@@ -3,7 +3,29 @@ const router = express.Router();
 
 router.use(express.static("public"));
 
-router.get("/play", (req, res) => {
+function generateRoomCode() {
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    for (let i = 0; i < 4; i ++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+let rooms = {}; //TODO: move
+
+router.post("/create", (req, res) => {
+    let roomId = generateRoomCode();
+    rooms[roomId] = {};
+    return res.json({roomId});
+});
+
+router.get("/:roomId", (req, res) => {
+    let { roomId } = req.params;
+    if (!rooms.hasOwnProperty(roomId)) {
+        return res.status(404).send();
+    }
+    rooms[roomId] = [];
     res.sendFile("public/game/chess.html", { root: process.cwd() });
 });
 
--- app/server.js
+++ app/server.js
@@ -1,15 +1,27 @@
-let express = require("express");
-let { Pool } = require("pg");
-
 // make this script's dir the cwd
 // b/c npm run start doesn't cd into src/ to run this
 // and if we aren't in its cwd, all relative paths will break
 process.chdir(__dirname);
 
-let port = 3000;
+const express = require("express");
+const { Pool } = require("pg");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
 let host;
-let databaseConfig;
+const port = 3000;
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
 // fly.io sets NODE_ENV to production automatically, otherwise it's unset when running locally
+let databaseConfig;
 if (process.env.NODE_ENV == "production") {
 	host = "0.0.0.0";
 	databaseConfig = { connectionString: process.env.DATABASE_URL };
@@ -19,13 +31,9 @@ if (process.env.NODE_ENV == "production") {
 	databaseConfig = { PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT };
 }
 
-let app = express();
-app.use(express.json());
-app.use(express.static("public"));
-
 let pool = new Pool(databaseConfig);
 pool.connect().then(() => {
-	console.log("Connected to db");
+	console.log("Connected to db"); 
 });
 
 const authRoutes = require("./routes/auth");
@@ -34,11 +42,43 @@ const gameRoutes = require("./routes/game");
 app.use("/auth", authRoutes);
 app.use("/game", gameRoutes);
 
-//TODO: This is basically a try-catch block. If a user tries to access a page taht doesnt exist, they just default go to the signup page.
-app.get("*", (req, res) => {
-    res.redirect("/auth/signup");
+// sockets from jason
+
+let rooms = {};
+
+io.on("connection", (socket) => {
+    let url = socket.handshake.headers.referer;
+    let pathParts = url.split("/");
+    let roomId = pathParts[pathParts.length - 1];
+
+    if (!rooms.hasOwnProperty(roomId)) {
+        return;
+    }
+
+    rooms[roomId][socket.id] = socket;
+
+    socket.on("disconnect", () => {
+        delete rooms[roomId][socket.id];
+    });
+
+    socket.on("gameUpdate", ({ from, to }) => {
+        for (let otherSocket of Object.values(rooms[roomId])) {
+            if (otherSocket.id === socket.id){
+                continue;
+            }
+            otherSocket.emit("gameUpdate", { from, to });
+        }
+    });
 });
 
-app.listen(port, host, () => {
+//
+
+//TODO: This is basically a try-catch block. If a user tries to access a page taht doesnt exist, they just default go to the signup page.
+app.get("*", (req, res) => {
+    // res.redirect("/auth/signup");
+	res.redirect("/game/menu.html"); //TODO: add signup and login button to this redirect
+});
+
+server.listen(port, host, () => {
     console.log(`Listening at: http://${host}:${port}`);
 });
