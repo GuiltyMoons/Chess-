adac4cf567cfe9604070d50c8f18707d17943b33
Paragax <xagarap@gmail.com>
2024-09-05 23:04:27 -0400

    join button implemented

--- app/public/game/dashboard.js
+++ app/public/game/dashboard.js
@@ -8,3 +8,31 @@ match.addEventListener("click", async () => {
         console.error("There was an error creating the room:", error);
     }
 });
+
+let joinButton = document.getElementById("join");
+joinButton.addEventListener("click", () => {
+    let roomCodeInput = document.getElementById("roomCode").value;
+
+    if (roomCodeInput) {
+        window.location = `/game/${roomCodeInput}`;
+    } else {
+        alert("Please enter a room code.");
+    }
+});
+
+const modal = document.getElementById("howToPlayModal");
+const btn = document.getElementById("howToPlayBtn");
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
